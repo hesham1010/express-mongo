b7ec854be811305f7b3c0ffce2bff2c7bb692bd8
hesham1010 <hesham.gublootech@gmail.com>
2024-06-08 01:46:41 +0500

    authentication

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const mongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -22,7 +24,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
-
+const csrfProtectection = csrf();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -33,6 +35,8 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtectection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -45,7 +49,11 @@ app.use((req, res, next) => {
     })
     .catch((err) => console.log(err));
 });
-
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -55,18 +63,6 @@ app.use(errorController.get404);
 mongoose
   .connect(mongoDbUri)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Hesham",
-          email: "hesham@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
